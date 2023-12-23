1f1e4a93ce8423e50b7a057f4b84869f10cce3d3
Ahmed Abdelrahman <abusaadma7@gmail.com>
2023-12-23 22:20:32 +0300

    Basic connectivity working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Item{
+  int id;
+  String name;
+
+  Item({required this.id, required this.name});
+
+  factory Item.fromJson(Map<String, dynamic> json){
+    return switch (json) {
+      {
+        'itemId': int id,
+        'name': String name
+      } => Item(
+        id: id,
+        name: name
+      ),
+      _ => throw const FormatException('This aint an item!')
+    };
+  }
+}
 
 void main() {
   runApp(const MaterialApp(
@@ -7,8 +29,18 @@ void main() {
   ));
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  static const TextStyle tStyle = TextStyle(fontSize: 30);
+  static const TextStyle hStyle = TextStyle(fontSize: 30, color: Colors.white);
+
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Item> data = [Item(id: 15, name: 'loading...')];
 
   @override
   Widget build(BuildContext context) {
@@ -18,28 +50,25 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-        color: Colors.lightGreen,
-        margin: const EdgeInsets.all(10.0),
-        // child: Icon(
-        //   Icons.warning
-        // )
-        // child: ElevatedButton.icon(
-        //     onPressed: (){
-        //       print('You have been FREEZED!!!\n');
-        //     },
-        //     icon: const Icon(Icons.ac_unit_rounded),
-        //     label: const Text('FREEZE!'),
-        //     style: ButtonStyle(
-        //       backgroundColor: MaterialStateProperty.all(Colors.amber[600])
-        //     ),
-        child: IconButton(
-          onPressed: (){
-            print('You Pressed this Icon!');
-          },
-          icon: const Icon(Icons.gamepad)
-        )
+      body: ListView(
+        children: data.map(
+                (x) => Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Card(
+                    clipBehavior: Clip.antiAlias, //makes the top surface smooth
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                            color: Colors.blueAccent,
+                            child: Text('Id: ${x.id}', style: HomeScreen.hStyle)
+                        ),
+                        Text('Name: ${x.name}', style: HomeScreen.tStyle),
+                      ],
+                    )
+                  ),
+                )
+        ).toList(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
@@ -47,6 +76,29 @@ class HomeScreen extends StatelessWidget {
         child: const Text('Click!'),
       ),
     );
+  }
+
+  void fetchAndUpdate() async {
+    try {
+      var resp = await http.get(
+          Uri.parse('http://192.168.100.19:5230/api/item'));
+
+      List<Item> list = (jsonDecode(resp.body) as List).map((x) => Item.fromJson(x)).toList();
+      //List<Item> list = [Item(id: 1, name:'Banana'), Item(id: 2, name: 'BlueBerry'), Item(id: 3, name: 'Orange')];
+      setState(() {
+        data = list + list + list + list; //artificially make the list long
+      });
+    }catch(e){
+      setState((){
+        data = [Item(id:400,name:e.toString())];
+      });
+    }
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    fetchAndUpdate();
   }
 }
 
